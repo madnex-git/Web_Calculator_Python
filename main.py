3e14f58e535fe00c78a3dd1330318001a6ee8004
Sabin <builderbet2018@gmail.com>
2025-10-26 22:16:50 +0545

    Simple HTML setup. Got the backend working with flask for handling two num inputs and returning the resul.

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-import flask
+from flask import Flask, render_template, request
 
-print("Hello world")
-print("Hello git")
-print("Hello hello")
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Calculator!"
+
+@app.route("/calculator", methods=["GET", "POST"])
+def calculator():
+    result=""
+    if request.method == "POST":
+        num1 = float(request.form.get("num1", 0))
+        num2 = float(request.form.get("num2", 0))
+        operation = request.form.get("operation")
+
+        if operation == "add":
+            result = num1 + num2
+        elif operation == "subtract":
+            result = num1 - num2
+        elif operation == "multiply":
+            result = num1 * num2
+        elif operation == "divide":
+            result = num1 / num2
+
+    return render_template("calculator.html", result = result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
